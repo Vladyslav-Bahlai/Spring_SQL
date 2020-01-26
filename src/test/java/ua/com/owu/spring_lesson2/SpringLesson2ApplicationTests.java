becc82b1c14132510991af1a9bd218c0889c48f0
Vlad Bahlai <bahlay.v@gmail.com>
2020-01-26 19:04:23 +0200

    Initial commit

---
+++ src/test/java/ua/com/owu/spring_lesson2/SpringLesson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.spring_lesson2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
